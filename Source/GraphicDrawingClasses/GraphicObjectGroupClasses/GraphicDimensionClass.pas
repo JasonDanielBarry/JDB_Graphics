22eaf856b65cb13525ecfd4ab9b154ea3d3fdb34
JasonBarry <jasondanielbarry512@gmail.com>
2025-05-28 23:12:08 +0200

    Update GraphicDimensionClass.pas
    (1) working on graphic dimension class

--- Source/GraphicDrawingClasses/GraphicObjectGroupClasses/GraphicDimensionClass.pas
+++ Source/GraphicDrawingClasses/GraphicObjectGroupClasses/GraphicDimensionClass.pas
@@ -2,6 +2,15 @@ unit GraphicDimensionClass;
 
 interface
 
+    uses
+        GraphicObjectGroupClass
+        ;
+
+    type
+        TGraphicDimension = class(TGraphicObjectGroup)
+            asdf
+        end;
+
 implementation
 
 end.
