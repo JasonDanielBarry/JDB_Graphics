248cfb6e30a9ed08d05ac32899058907bf99d1af
JasonBarry <jasondanielbarry512@gmail.com>
2025-07-18 16:25:28 +0200

    (1) fixed search paths (2) working on custom D2D canvas project

---
+++ Source/TestJDBD2DCanvasMainForm.dfm
@@ -0,0 +1,26 @@
+object JDB_D2D_Form: TJDB_D2D_Form
+  Left = 0
+  Top = 0
+  Caption = 'JDB_D2D_Form'
+  ClientHeight = 721
+  ClientWidth = 1334
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object PaintBox1: TPaintBox
+    Left = 0
+    Top = 0
+    Width = 1334
+    Height = 721
+    Align = alClient
+    OnPaint = PaintBox1Paint
+    ExplicitLeft = 312
+    ExplicitTop = 320
+    ExplicitWidth = 105
+    ExplicitHeight = 105
+  end
+end
---
+++ Source/TestJDBD2DCanvasMainForm.pas
@@ -0,0 +1,32 @@
+unit TestJDBD2DCanvasMainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls,
+  jdb;
+
+type
+  TJDB_D2D_Form = class(TForm)
+    PaintBox1: TPaintBox;
+    procedure PaintBox1Paint(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  JDB_D2D_Form: TJDB_D2D_Form;
+
+implementation
+
+{$R *.dfm}
+
+procedure TJDB_D2D_Form.PaintBox1Paint(Sender: TObject);
+    begin
+        asdf
+    end;
+
+end.
