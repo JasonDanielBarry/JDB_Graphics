6b63aa87748e251ad9971641c9b898e26312b462
JasonBarry <jasondanielbarry512@gmail.com>
2025-07-21 16:41:39 +0200

    (1) renamed and reorganised folders

---
+++ Source/GraphicEntityClasses/GraphicEntityTypes.pas
@@ -0,0 +1,13 @@
+unit GraphicEntityTypes;
+
+interface
+
+    type
+        {$SCOPEDENUMS ON}
+            EArrowOrigin            = (aoHead = 0, aoTail = 1);
+            EArrowGroupDirection    = (agdUp = 0, agdDown = 1, agdLeft = 2, agdRight = 3, agdNormal = 4, agdUserDefined = 5);
+        {$SCOPEDENUMS OFF}
+
+implementation
+
+end.
